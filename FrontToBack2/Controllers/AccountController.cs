2bee10f855c4999c032920263a43a2edb507bd6b
Togrul85 <toghrulkhs@code.edu.az>
2023-03-11 13:31:51 +0400

    start

---
+++ FrontToBack2/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using FrontToBack2.Models;
+using FrontToBack2.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+      
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser user = new();
+            user.Email = register.Email;
+            user.Fullname = register.Fullname;
+            user.UserName = register.Username;
+            var result = await _userManager.CreateAsync(user, register.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(register);
+            }
+
+            //add role 
+
+            //sign in
+
+            await _signInManager.SignInAsync(user, true);
+
+
+
+
+
+
+
+            return RedirectToAction("index", "home");
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
